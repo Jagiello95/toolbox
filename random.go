0ee5920280d977af6bb635fb96a027d236b16423
Andrzej <and.jagiello@gmail.com>
2022-05-09 22:10:56 +0200

    fixes

--- random.go
+++ random.go
@@ -9,17 +9,19 @@ import (
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+type Random struct{}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 // RandomInt generates a random integer between min and max
-func RandomInt(min, max int64) int64 {
+func (r *Random) RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
 // RandomString generates a random string of length n
-func RandomString(n int) string {
+func (r *Random) RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
 
@@ -32,23 +34,23 @@ func RandomString(n int) string {
 }
 
 // RandomOwner generates a random owner name
-func RandomOwner() string {
-	return RandomString(6)
+func (r *Random) RandomOwner() string {
+	return r.RandomString(6)
 }
 
 // RandomMoney generates a random amount of money
-func RandomMoney() int64 {
-	return RandomInt(0, 1000)
+func (r *Random) RandomMoney() int64 {
+	return r.RandomInt(0, 1000)
 }
 
 // RandomCurrency generates a random currency code
-func RandomCurrency() string {
+func (r *Random) RandomCurrency() string {
 	currencies := []string{"USD", "EUR", "CAD"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
 
 // RandomEmail generates a random email
-func RandomEmail() string {
-	return fmt.Sprintf("%s@email.com", RandomString(6))
+func (r *Random) RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", r.RandomString(6))
 }
